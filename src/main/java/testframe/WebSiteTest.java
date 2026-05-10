1de35000b94cfe8df66294c5fe12efbdf711f7f1
server pc <server pc@intech>
2026-05-10 13:12:24 +0530

    changes2

--- src/main/java/testframe/WebSiteTest.java
+++ src/main/java/testframe/WebSiteTest.java
@@ -1,6 +1,8 @@
 package testframe;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebSiteTest {
@@ -9,8 +11,16 @@ public class WebSiteTest {
 		// TODO Auto-generated method stub
 
 		WebDriver d=new ChromeDriver();
-		d.get("https://www.flipkart.com/");
+		d.get("https://proposalfinder.com/");
+		d.manage().window().fullscreen();
 		
+		//Syntax of xpath:- 
+		//tagname[@attribute='attribute name']
+		
+		//a[@id='open-login-modal']
+		
+		WebElement login=d.findElement(By.xpath("//a[@id='open-login-modal']"));
+		login.click();
 		
 		
 	}
